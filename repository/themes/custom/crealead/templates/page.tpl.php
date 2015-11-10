fff93cc1ecb37d441726ab5f1f35b178a99fe31a
Yvan Douenel <y.douenel@coopernet.fr>
2015-11-10 11:23:43 +0100

    page.tpl.php modified.

--- repository/themes/custom/crealead/templates/page.tpl.php
+++ repository/themes/custom/crealead/templates/page.tpl.php
@@ -73,18 +73,20 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+<header id="navbar" role="banner" class="<?php print $navbar_classes; ?> transparent">
   <div class="container">
     <div class="navbar-header">
       <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        <h1 id="h1-logo">
+      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>">
+        <img src="<?php print $logo; ?>" alt="Crealead : Coopérative d'entrepreneurs à Montpellier" />
       </a>
+        </h1>
       <?php endif; ?>
 
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
+      <?php /*if (!empty($site_name)): */?><!--
+      <a class="name navbar-brand" href="<?php /*print $front_page; */?>" title="<?php /*print t('Home'); */?>"><?php /*print $site_name; */?></a>
+      --><?php /*endif; */?>
 
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
