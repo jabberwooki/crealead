a553eca89935c087878a1f3d28249c5c8796bcd5
Yvan Douenel <y.douenel@coopernet.fr>
2015-11-20 19:21:57 +0100

    views-view-unformatted--news--block-news-2-3.tpl.php added to add specific div and classes for the grid.

--- repository/themes/custom/crealead/templates/views-view-unformatted--news--block-news-2-3.tpl.php
+++ repository/themes/custom/crealead/templates/views-view-unformatted--news--block-news-2-3.tpl.php
@@ -10,8 +10,10 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
+<div class="row">
 <?php foreach ($rows as $id => $row): ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .' col-md-6 boby"';  } ?>>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .' col-md-6"';  } ?>>
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
+</div>
