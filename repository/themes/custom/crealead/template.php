8f9f59a71e93611c75b3f7dfa35c3e28bc3b4ef8
Yvan Douenel <y.douenel@coopernet.fr>
2015-11-20 12:22:00 +0100

    template.php updated : custom js file added.

--- repository/themes/custom/crealead/template.php
+++ repository/themes/custom/crealead/template.php
@@ -4,12 +4,27 @@
  * @file
  * template.php
  */
-/*
+/**
  * Remove width and height attributes to images tags
- * */
+ */
 function crealead_preprocess_image(&$variables) {
   foreach (array('width', 'height') as $key) {
     unset($variables[$key]);
     unset($variables[$key]);
   }
+}
+
+/**
+ * Add js to anmimate crealead key values
+*/
+if (drupal_is_front_page()){
+  drupal_add_js(drupal_get_path('theme', 'crealead') . '/js/jquery.animateNumber.js', array(
+    'type' => 'file',
+    'group' => JS_THEME,
+  ));
+  drupal_add_js(drupal_get_path('theme', 'crealead') . '/js/animate_numbers.js', array(
+    'type' => 'file',
+    'group' => JS_THEME,
+  ));
+
 }
