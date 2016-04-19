4407e3e2b3f57a8c7937f11b82ba28b2be52cad4
Christophe Espiau <christophe.espiau@korora.fr>
2016-04-19 12:03:01 +0200

    crealead_info_meetings up: US 'Rentrer les données complètes des candidats' completed as requested in https://trello.com/c/4izVmiGm/110-rentrer-les-donnees-completes-des-candidats.

--- repository/modules/features/crealead_info_meetings/js/crealead_info_meetings.js
+++ repository/modules/features/crealead_info_meetings/js/crealead_info_meetings.js
@@ -3,9 +3,36 @@
  */
 
 (function ($) {
-  Drupal.behaviors.advanced_pages = {
+  Drupal.behaviors.info_meetings = {
     attach: function (context, settings) {
-      // First we hide non used dependent info fields.
+      ////////// Reorganisation of STATUT checkboxes field + complementary info fields. //////////
+      // 1) First, we wrap checkboxes into div and we add a second informational div with the following pattern;
+      // <DIV> checkbox </DIV><DIV> area for complementary info </DIV>
+      $("#edit-field-registrant-status-und").children().each(function() {
+        termId = $(this).children('input').val();
+        var content = $(this).html();
+        var newContent = '<div id="'
+          + settings.crealead_registrant_statuses[termId]
+          + '-checkbox">' + content
+          + '</div><div id="'
+          + settings.crealead_registrant_statuses[termId]
+          + '-comp-info"></div>';
+        $(this).html(newContent);
+      })
+
+      // 2) Then we catch complementary info fields
+      // and put them into their respective reserved div.
+      rsaNumberField = $("#edit-field-beneficiary-number").wrap('<p>').parent().html();
+      $("#edit-field-beneficiary-number").parent().remove();
+      $("#rsa-comp-info").html(rsaNumberField);
+      unemploymentDateField = $("#edit-field-unemployment-date").wrap('<p>').parent().html();
+      $("#edit-field-unemployment-date").parent().remove();
+      $("#pol-comp-info").html(unemploymentDateField);
+      otherSpecifyField = $("#edit-field-other-specify").wrap('<p>').parent().html();
+      $("#edit-field-other-specify").parent().remove();
+      $("#aut-comp-info").html(otherSpecifyField);
+
+      // 3) Next, we must hide complementary info fields on first display.
       var checkedValue = $('input[name="field_registrant_status[und]"]:checked').parent().text().charAt(4);
       console.log('value = ' + checkedValue);
       if (checkedValue != 'R') {
@@ -18,40 +45,38 @@
         $("div[id^=edit-field-other-specify]").hide();
       }
 
-      $('input[name="field_registrant_status[und]"]:radio').click(function () {
-        var firstLetter = $('input[name="field_registrant_status[und]"]:radio:checked').parent().text().charAt(4);
-        var duration = 500;
-        if (firstLetter == 'R') {
-          $("input[id^=edit-field-unemployment-date]").val('');
-          $("div[id^=edit-field-unemployment-date]").hide(duration);
-          $("input[id^=edit-field-other-specify]").val('');
-          $("div[id^=edit-field-other-specify]").hide(duration);
-
-          $("div[id^=edit-field-beneficiary-number]").show(duration);
+      // 4) And eventually, we code chechboxes and complementary info fields behavior.
+      // We must show complementary info field, if any, when corresponding checkbox is checked.
+      // We must empty it and hide it when corresponding checkbox is unchecked.
+      $("#edit-field-registrant-status-und input").on('change', function () {
+        var parentId = $(this).parent().attr("id");
+        var duration = 400;
+        if (parentId == 'rsa-checkbox') {
+          if ($(this).is(':checked')) {
+            $("#edit-field-beneficiary-number").show(duration);
+          }
+          else {
+            $("input[id^=edit-field-beneficiary-number]").val('');
+            $("#edit-field-beneficiary-number").hide(duration);
+          }
         }
-        else if (firstLetter == 'P') {
-          $("input[id^=edit-field-beneficiary-number]").val('');
-          $("div[id^=edit-field-beneficiary-number]").hide(duration);
-          $("input[id^=edit-field-other-specify]").val('');
-          $("div[id^=edit-field-other-specify]").hide(duration);
-
-          $("div[id^=edit-field-unemployment-date]").show(duration);
+        else if (parentId == 'pol-checkbox') {
+          if ($(this).is(':checked')) {
+            $("#edit-field-unemployment-date").show(duration);
+          }
+          else {
+            $("input[id^=edit-field-unemployment-date]").val('');
+            $("#edit-field-unemployment-date").hide(duration);
+          }
         }
-        else if (firstLetter == 'A') {
-          $("input[id^=edit-field-beneficiary-number]").val('');
-          $("div[id^=edit-field-beneficiary-number]").hide(duration);
-          $("input[id^=edit-field-unemployment-date]").val('');
-          $("div[id^=edit-field-unemployment-date]").hide(duration);
-
-          $("div[id^=edit-field-other-specify]").show(duration);
-        }
-        else {
-          $("input[id^=edit-field-beneficiary-number]").val('');
-          $("div[id^=edit-field-beneficiary-number]").hide(duration);
-          $("input[id^=edit-field-unemployment-date]").val('');
-          $("div[id^=edit-field-unemployment-date]").hide(duration);
-          $("input[id^=edit-field-other-specify]").val('');
-          $("div[id^=edit-field-other-specify]").hide(duration);
+        else if (parentId == 'aut-checkbox') {
+          if ($(this).is(':checked')) {
+            $("#edit-field-other-specify").show(duration);
+          }
+          else {
+            $("input[id^=edit-field-other-specify]").val('');
+            $("#edit-field-other-specify").hide(duration);
+          }
         }
       });
     }
