9b421a7a95a3bcf36b9503bd684ef2d429ef6eca
Yvan Douënel <y.douenel@coopernet.fr>
2016-06-24 15:50:38 +0200

    crealead_sector_brands_menu.js up to improve open-close mecanism in sector brands.

--- repository/modules/features/crealead_brands/js/crealead_sector_brands_menu.js
+++ repository/modules/features/crealead_brands/js/crealead_sector_brands_menu.js
@@ -31,16 +31,16 @@
           $arrow_down.insertAfter('#introduction.sector-brands-header');
           // show the headband after a click on the div by adding or removing class sector-brands-desc-open
           $arrow_down.click(function(){
-            $('#open-close-sector-brand').toggleClass( "sector-brands-desc-open", 1000);
+            $('#introduction.sector-brands-header .views-field-description').toggleClass( "sector-brands-desc-open");
             $('#introduction.sector-brands-header').toggleClass( "sector-brands-desc-open", 1000);
-            $('#introduction.sector-brands-header .views-field-description').toggleClass( "sector-brands-desc-open",1000);
+
 
           });
         }
       }
       // Manage the height of news
-      if ($(".view-display-id-sector_brand_news .views-row").length) {
-        $('.view-display-id-sector_brand_news .views-row').matchHeight();
+      if ($(".view-display-id-sector_brand_news .views-row .field-name-body").length) {
+        $('.view-display-id-sector_brand_news .views-row .field-name-body').matchHeight();
       }
 
     }
