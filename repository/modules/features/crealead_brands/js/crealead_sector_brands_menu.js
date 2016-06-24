d8ffecd5956a1586b52eb0e6119962891c310174
Yvan Douënel <y.douenel@coopernet.fr>
2016-06-24 12:03:51 +0200

    Feature brands up : page sector brands modified to match design.

--- repository/modules/features/crealead_brands/js/crealead_sector_brands_menu.js
+++ repository/modules/features/crealead_brands/js/crealead_sector_brands_menu.js
@@ -10,6 +10,7 @@
         e.preventDefault();
       });
 
+      // manage open-close mecanism on sector brands
       if ($(".view-sector-brands .views-field-description .field-content").length){
         // add class to main container to manage top margin and padding
         $("body > .main-container").addClass("main-container-sector-brands");
@@ -30,14 +31,18 @@
           $arrow_down.insertAfter('#introduction.sector-brands-header');
           // show the headband after a click on the div by adding or removing class sector-brands-desc-open
           $arrow_down.click(function(){
-            $('#introduction.sector-brands-header .views-field-description').toggleClass( "sector-brands-desc-open" );
-            $('#introduction.sector-brands-header').toggleClass( "sector-brands-desc-open" );
-            $('#open-close-sector-brand').toggleClass( "sector-brands-desc-open" );
+            $('#open-close-sector-brand').toggleClass( "sector-brands-desc-open", 1000);
+            $('#introduction.sector-brands-header').toggleClass( "sector-brands-desc-open", 1000);
+            $('#introduction.sector-brands-header .views-field-description').toggleClass( "sector-brands-desc-open",1000);
+
           });
         }
-
-
       }
+      // Manage the height of news
+      if ($(".view-display-id-sector_brand_news .views-row").length) {
+        $('.view-display-id-sector_brand_news .views-row').matchHeight();
+      }
+
     }
   };
 }(jQuery));
