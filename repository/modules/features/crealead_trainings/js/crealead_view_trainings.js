36e5c12a1d59cc1a5fbff02414007fdd01ef6752
Yvan Douenel <y.douenel@coopernet.fr>
2017-01-07 20:06:33 +0100

    crealead_trainings.module up to add js matchHeight behaviour.

--- repository/modules/features/crealead_trainings/js/crealead_view_trainings.js
+++ repository/modules/features/crealead_trainings/js/crealead_view_trainings.js
@@ -1,3 +1,4 @@
 jQuery(function($) {
-  $('.group-training-data .col-md-3 ').matchHeight();
+  $('.group-training-data .col-md-3').matchHeight();
+  $('.page-formations .group-wrapper-teaser').matchHeight();
 });
