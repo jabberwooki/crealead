bd5daff73789ee0e6917e1f816028c61a19fe8ad
Yvan Douenel <y.douenel@coopernet.fr>
2017-02-08 16:07:16 +0100

    Feature trainings up to add infinite scroll.

--- repository/modules/features/crealead_trainings/js/crealead_view_trainings.js
+++ repository/modules/features/crealead_trainings/js/crealead_view_trainings.js
@@ -1,4 +1,8 @@
 jQuery(function($) {
   $('.group-training-data .col-md-3').matchHeight();
   $('.page-formations .group-wrapper-teaser').matchHeight();
+  $( document ).ajaxComplete(function() {
+    $('.group-training-data .col-md-3').matchHeight();
+    $('.page-formations .group-wrapper-teaser').matchHeight();
+  });
 });
