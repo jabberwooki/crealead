23165c92a1add67dbad3f2633c19cbcb930be96f
Yvan Douenel <y.douenel@coopernet.fr>
2017-05-16 16:04:55 +0200

    match-actu-height.js up.

--- repository/themes/custom/crealead/less/js/match-actu-height.js
+++ repository/themes/custom/crealead/less/js/match-actu-height.js
@@ -1,10 +1,12 @@
 jQuery(function($) {
   $('.red-title').matchHeight();
   $('.line-bottom-red').matchHeight();
+  $('.text-actu').matchHeight();
   $('.view-display-id-page_news_list .node-news').matchHeight();
   $( document ).ajaxComplete(function() {
     $('.red-title').matchHeight();
     $('.line-bottom-red').matchHeight();
+    $('.text-actu').matchHeight();
     $('.view-display-id-page_news_list .node-news').matchHeight();
   });
 
