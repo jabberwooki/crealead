b2078cdc08d43f6279624a39a16c3de5ea920f6b
Christophe Espiau <christophe.espiau@gmail.com>
2017-11-27 14:11:40 +0100

    crealead_needs up: Closes story V3 S3-13: Needs table display reorganised.

--- repository/modules/features/crealead_needs/js/crealead_needs.js
+++ repository/modules/features/crealead_needs/js/crealead_needs.js
@@ -42,16 +42,24 @@
 
       var initialClientValue = $('#edit-field-need-client-und').val();
 
+      var userBrand = settings.crealead_needs_user_brand;
+
       if (initialClientValue == '_none') {
         $('#edit-field-crealead-name').hide();
         $('.group-need-contact-infos').hide();
       }
       else {
-        if (client_uuids[initialClientValue] == '7b581fb4-6fcb-4be4-8f14-53f14dc4f3d4') { // Client externe.
-          $('#edit-field-crealead-name').hide();
+        if (client_uuids[initialClientValue] == 'b4346853-567b-448c-910d-0e79d75d4e99') { // Activité co-entrepreneur.
+          $('#field-crealead-name-add-more-wrapper label').text('Marque concernée');
+
+        }
+        else if (client_uuids[initialClientValue] == '7b581fb4-6fcb-4be4-8f14-53f14dc4f3d4') { // Client externe.
+          $('#field-crealead-name-add-more-wrapper label').text('Organisme');
+          //$('#edit-field-crealead-name').hide();
         }
         else if (client_uuids[initialClientValue] == '7a01c453-5ac3-42a6-a374-ee9d26137a2f') { // Crealead Structure
-          $('.group-need-contact-infos').hide();
+          $('#edit-field-crealead-name').hide();
+          $('.group-need-contact-infos').show();
         }
       }
 
@@ -76,15 +84,26 @@
             $('#edit-field-need-contact-details-und-0-value').val(initialContactDetails);
           }
           else {
+            $('#field-crealead-name-add-more-wrapper label').text('Personne concernée');
             $('#edit-field-crealead-name-und-0-value').val(connectedUser.name);
             $('#edit-field-need-contact-name-und-0-value').val(connectedUser.name);
             $('#edit-field-need-contact-details-und-0-value').val(connectedUser.mail);
 
-            if (selectedClientUuid == '7a01c453-5ac3-42a6-a374-ee9d26137a2f') { // Crealead Structure
+            if (selectedClientUuid == 'b4346853-567b-448c-910d-0e79d75d4e99') { // Activité co-entrepreneur
+              $('#field-crealead-name-add-more-wrapper label').text('Marque concernée');
+              if (userBrand) {
+                $('#edit-field-crealead-name-und-0-value').val(userBrand);
+              }
+              else {
+                $('#edit-field-crealead-name-und-0-value').val('');
+              }
+            }
+            else if (selectedClientUuid == '7a01c453-5ac3-42a6-a374-ee9d26137a2f') { // Crealead Structure
               $('#edit-field-need-contact-name-und-0-value').val('');
               $('#edit-field-need-contact-details-und-0-value').val('');
             }
             else if (selectedClientUuid == '7b581fb4-6fcb-4be4-8f14-53f14dc4f3d4') { // Client externe
+              $('#field-crealead-name-add-more-wrapper label').text('Organisme');
               $('#edit-field-crealead-name-und-0-value').val('');
               $('#edit-field-need-contact-name-und-0-value').val('');
               $('#edit-field-need-contact-details-und-0-value').val('');
@@ -98,12 +117,12 @@
           $('.group-need-contact-infos').hide(500);
         }
         else if (selectedClientUuid == '7b581fb4-6fcb-4be4-8f14-53f14dc4f3d4') { // Client externe
-          $('#edit-field-crealead-name').hide(250);
+          $('#edit-field-crealead-name').show(250);
           $('.group-need-contact-infos').show(500);
         }
         else if (selectedClientUuid == '7a01c453-5ac3-42a6-a374-ee9d26137a2f') { // Crealead Structure
-          $('#edit-field-crealead-name').show(500);
-          $('.group-need-contact-infos').hide(250);
+          $('#edit-field-crealead-name').hide(500);
+          $('.group-need-contact-infos').show(250);
         }
         else  {
           $('#edit-field-crealead-name').show(500);
