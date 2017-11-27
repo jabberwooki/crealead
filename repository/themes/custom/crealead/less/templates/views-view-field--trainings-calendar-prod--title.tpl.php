c07c5b058240942835d1cb9cce2210ed9e57b257
Christophe Espiau <christophe.espiau@gmail.com>
2017-11-27 16:20:23 +0100

    crealead_trainings up: template for session titles in Month view improved to display missing day number (J1, J2...).

--- repository/themes/custom/crealead/less/templates/views-view-field--trainings-calendar-prod--title.tpl.php
+++ repository/themes/custom/crealead/less/templates/views-view-field--trainings-calendar-prod--title.tpl.php
@@ -8,6 +8,18 @@
 ?>
 
 <?php
+  /*
+   * Le titre de la formation suivi du numéro du jour (dans la session) est généré dans le hook crealead_trainings_views_pre_render().
+   * Mais si on ne fait rien, ce template n'affiche que le titre de la formation, pas ne numéro du jour.
+   *
+   * Heureusement, toutes les données sont dans les variables transmises à la page :
+   *   $row->field_field_related_training[0]['rendered']['#markup'] contient Titre + Numéro jour.
+   *   $row->node_title contient le Titre seulement.
+   *   $output contient le titre formaté en tant que lien vers son node dans une colorbox.
+   *
+   * Donc, un petit coup de str_replace pour remplacer Titre par Titre + Numéro Jour dans la chaine formatée $output
+   */
+  $output = str_replace($row->field_field_related_training[0]['rendered']['#markup'], $row->node_title, $output);
   $funded = 0;
   $funded_logo_markup = '';
 
