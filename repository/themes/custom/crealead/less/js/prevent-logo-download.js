4eaa81caf79a9734b7ef15365c070831dc09b432
Yvan Douenel <y.douenel@coopernet.fr>
2018-03-15 09:39:33 +0100

    prevent-logo-download.js to avoid jquery-ui.

--- repository/themes/custom/crealead/less/js/prevent-logo-download.js
+++ repository/themes/custom/crealead/less/js/prevent-logo-download.js
@@ -1,32 +1,28 @@
 jQuery(function ($) {
-
-
-  $.extend({
-    alert: function (message, title) {
-      $("<div></div>").dialog({
-        buttons: {
-          "Ok": function () {
-            $(this).dialog("close");
-          }
-        },
-        close: function (event, ui) {
-          $(this).remove();
-        },
-        resizable: false,
-        title: title,
-        modal: true
-      }).html(message);
+  console.log("alert-logo");
+  var msg =
+    "La fonctionnalité de téléchargement du logo a été supprimée pour inciter les co-entrepreneurs de Crealead à " +
+    "utiliser le logo en haute définitition qui se trouve dans la rubrique " +
+    "<a style=\"color:#A00C2F;\" href=\"/espace-co-entrepreneur/documents\">" +
+    " Espace co-entrepreneurs > Documents.</a>";
+  var close_logo = $('<div />', {
+    id: 'close-logo',
+    click: function (e) {
+      e.preventDefault();
+      $("#alert-logo").hide(0);
     }
   });
+
+  var alert_logo = $('<div />', {
+    id: 'alert-logo',
+    html: msg
+  });
+  alert_logo.prependTo('body');
+  close_logo.prependTo('#alert-logo');
+  alert_logo.hide();
   $('body').on('contextmenu', '#h1-logo img', function (e) {
-    /*alert("La fonctionnalité de téléchargement du logo a été supprimée pour inciter les co-entrepreneurs de Crealead à " +
-     "utiliser le logo en haute définitition qui se trouve dans la rubrique " +
-     "<a href=\"/espace-co-entrepreneur/documentsEspace co-entrepreneur\">" +
-     " Espace co-entrepreneurs > Documents.</a>");*/
-    $.alert("La fonctionnalité de téléchargement du logo a été supprimée pour inciter les co-entrepreneurs de Crealead à " +
-      "utiliser le logo en haute définitition qui se trouve dans la rubrique " +
-      "<a style=\"color:#A00C2F;\" href=\"/espace-co-entrepreneur/documents\">" +
-      " Espace co-entrepreneurs > Documents.</a>", "Logo Crealead pour le web");
+    alert_logo.show();
     return false;
   });
 });
+
