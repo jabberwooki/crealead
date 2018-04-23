c1fd060a4e067a9a987f4272a02208c70a3e050e
Christophe Espiau <christophe.espiau@gmail.com>
2018-04-23 17:40:44 +0200

    crealead_trainings up: Recommendation mechanism added to brand's training list tab (solutions).

--- repository/modules/features/crealead_trainings/js/crealead_training_list.js
+++ repository/modules/features/crealead_trainings/js/crealead_training_list.js
@@ -3,7 +3,7 @@
  */
 
 (function ($) {
-  Drupal.behaviors.trainingFull = {
+  Drupal.behaviors.trainingList = {
     attach: function (context, settings) {
       // Add node wrapper for ajax refresh purposes
       $('.node-training .field-name-training-nid .field-item.even').each(function () {
