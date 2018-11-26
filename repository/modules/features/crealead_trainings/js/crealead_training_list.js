bb23b939c4e5a1c10779b70a875411464a8f34fd
Christophe Espiau <christophe.espiau@gmail.com>
2018-11-26 09:26:37 +0100

    crealead_trainings up: V3 7-15 - Trainings list filters improved to highlight crealead offers.

--- repository/modules/features/crealead_trainings/js/crealead_training_list.js
+++ repository/modules/features/crealead_trainings/js/crealead_training_list.js
@@ -10,6 +10,45 @@
         var trainingNid = $(this).text();
         $(this).parents().eq(5).wrap('<div id="node-training-item-wrapper-' + trainingNid + '" class="training-item-wrapper"></div>');
       });
+
+      // CUSTOM FILTERS HANDLING.
+      var customFiltersExist = false;
+      var filtersHtml = '';
+
+      // Crealead price
+      if ($('#edit-field-custom-crealead-price-value').length) {
+        customFiltersExist = true;
+        var crealeadPriceHtml = $('div.views-submit-button .form-item-field-custom-crealead-price-value').html();
+        filtersHtml += '<div class="views-widget">' + crealeadPriceHtml + '</div>';
+        $('div.views-submit-button .form-item-field-custom-crealead-price-value').remove();
+      }
+
+      // Coopins
+      if ($('#edit-field-custom-coopins-value').length) {
+        customFiltersExist = true;
+        var coopinsHtml = $('div.views-submit-button .form-item-field-custom-coopins-value').html();
+        filtersHtml += '<div class="views-widget">' + coopinsHtml + '</div>';
+        $('div.views-submit-button .form-item-field-custom-coopins-value').remove();
+      }
+
+      // Crealead funded
+      if ($('#edit-field-custom-crealead-funded-value').length) {
+        customFiltersExist = true;
+        var crealeadFundecHtml = $('div.views-submit-button .form-item-field-custom-crealead-funded-value').html();
+        filtersHtml += '<div class="views-widget">' + crealeadFundecHtml + '</div>';
+        $('div.views-submit-button .form-item-field-custom-crealead-funded-value').remove();
+      }
+
+      // Moving filters to the right place in form.
+      if (customFiltersExist) {
+        $('div.views-submit-button').before(
+          '<div id="edit-custom-coopins-value-wrapper" class="views-exposed-widget">'
+          + '<div>&nbsp;</div>'
+          + '<div class="views-widget">'
+          + filtersHtml
+          + '</div></div>'
+        );
+      }
     }
   };
 }(jQuery));
